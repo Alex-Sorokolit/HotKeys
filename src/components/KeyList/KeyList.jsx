867cff95808570de6720e05637a2eb45725edd92
Alex-Sorokolit <104554703+Alex-Sorokolit@users.noreply.github.com>
2023-02-05 00:03:09 +0200

    menuDontWork

--- src/components/KeyList/KeyList.jsx
+++ src/components/KeyList/KeyList.jsx
@@ -1,23 +1,37 @@
 import { Item } from 'components/Item/Item';
 import css from './KeyList.module.css';
-
+import { useState } from 'react';
 import { useToggle } from 'hooks/useToggle';
-import { ContextMenu } from 'components/ContextMenu/ContextMenu';
-import { Confirm } from 'components/Confirm/Confirm';
 import { useConfirmToggle } from 'hooks/useToggle';
-
 export const KeysList = ({ visibleShortCuts, deleteShortcut }) => {
+  const [selectedItemIdx, setSelectedItemIdx] = useState(null);
   const { isOpen, toggle, close } = useToggle(false);
   const { isConfirmOpen, confirmToggle, confirmClose } =
     useConfirmToggle(false);
 
+  const selectItem = id => {
+    console.log('click');
+    setSelectedItemIdx(id);
+  };
   return (
     <>
       {visibleShortCuts.length > 0 && (
         <section>
           <ul className={css.list}>
             {visibleShortCuts.map(item => (
-              <Item key={item.id} item={item} deleteShortcut={deleteShortcut} />
+              <Item
+                key={item.id}
+                item={item}
+                deleteShortcut={deleteShortcut}
+                selectedItemIdx={selectedItemIdx}
+                isOpen={isOpen}
+                toggle={toggle}
+                close={close}
+                isConfirmOpen={isConfirmOpen}
+                confirmToggle={confirmToggle}
+                confirmClose={confirmClose}
+                selectItem={selectItem}
+              />
             ))}
           </ul>
         </section>
--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useToggle = (initialState = false) => {
   const [isOpen, setIsOpen] = useState(initialState);
+  useEffect(() => {
+    console.log(isOpen);
+  }, [isOpen]);
   // const open = () => setIsOpen(true);
   const close = () => setIsOpen(false);
   const toggle = () => setIsOpen(isOpen => !isOpen);
